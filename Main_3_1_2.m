c2290fea57aa4f787fcb96353ee7886f0cbb0b83
sinorious <erndwein@kth.se>
2018-03-21 13:19:48 +0100

    Final changes and different plotting options

--- Main_3_1_2.m
+++ Main_3_1_2.m
@@ -3,17 +3,17 @@ clear all; close all; clc
 %% Import and Plot original data
 
 importdata('binMNIST');
-for i = 1:20
-    %original data
-    figure(1)
-    subplot(4,5,i)
-    imshow(reshape(bindata_trn(i,:),28,28)')
-end
+% for i = 1:20
+%     %original data
+%     figure(1)
+%     subplot(4,5,i)
+%     imshow(reshape(bindata_trn(i,:),28,28)')
+% end
 %% Define constants
 
 epochs = 75;
 %hidden_size = [50 75 100 150];
-hidden_size = 150;
+hidden_size = 100;
 bindata_err = zeros(size(hidden_size));
 
 %% Train the network
@@ -31,6 +31,7 @@ bindata_tst = bindata_tst';
 
 for h = 1:length(hidden_size)
     autoenc = trainAutoencoder(bindata_trn,hidden_size(h),enc);
+    weights = autoenc.EncoderWeights;
     traindata_rec = predict(autoenc, bindata_trn);
     %bindata_err(h) = mean(mean((bindata_trn - bindata_rec).^2));
     
@@ -38,24 +39,28 @@ for h = 1:length(hidden_size)
     testdata_rec = predict(autoenc, bindata_tst);
     
     %Plot the images
-    digit = 0:9;
     %find targets for each digit
-    for k = 1:length(digtargets_tst)
-        digidata = bindata_tst(:,find(digtargets_tst(i) == digit,1));
-        recdata = testdata_rec(:,find(digtargets_tst(i) == digit,1));
-    end
-    
-    for i = 1:10
-        figure(h+1)
-        subplot(5,5,i)
-        imshow(reshape(digidata(:,i),28,28)')
-        subplot(5,5,10+i)
-        imshow(reshape(recdata(:,i),28,28)')
-        hold on
-    end
-    hold off
+%     digdata = zeros(784,10);
+%     recdata = zeros(784,10);
+%     plotvec = [1 6 11 16 21 4 9 14 19 24];
+%     for d = 1:10                             %digits
+%         k = find(digtargets_tst == d-1,1);   %indices for every digit
+%         digdata(:,d) = bindata_tst(:,k);
+%         recdata(:,d) = testdata_rec(:,k);
+%         
+%         figure(h+1)
+%         subplot(5,5,plotvec(d))
+%         imshow(reshape(digdata(:,d),28,28)')
+%         subplot(5,5,plotvec(d)+1)
+%         imshow(reshape(recdata(:,d),28,28)')
+%     end
 end
 
 
 %% Plot the weights
-%autoenc.EncoderWeights 
+for i = 1:hidden_size
+    figure(3)
+    subplot(10,10,i)
+    imshow(reshape(weights(i,:),28,28)')
+    title(['Node ',num2str(i)],'FontSize',6)
+end
