b3e905a6c27bb1c0845bfc5d865d935160b730e0
sinorious <erndwein@kth.se>
2018-03-22 23:21:16 +0100

    done, soft tuning done by whole network supervised learning

--- Main_3_2_2.m
+++ Main_3_2_2.m
@@ -4,11 +4,11 @@ clear all; close all; clc
 importdata('binMNIST');
 
 %% Define constants and Parameters
-epochs = 50;
+epochs = 75;
 eta = 0.01;
-hidden_size1 = 300;
-hidden_size2 = 250;
-hidden_size3 = 200;
+hidden_size1 = 150;
+hidden_size2 = 125;
+hidden_size3 = 100;
 
 %network features
 enc.MaxEpochs = epochs;
@@ -36,13 +36,14 @@ features2 = encode(autoenc2,features1);
 autoenc3 = trainAutoencoder(features2,hidden_size3,enc);
 features3 = encode(autoenc3,features2);
 
-%decode wit the last autoencoder
+%decode with the last autoencoder
 %features = decode(autoenc3,features3);
 
 %classification layer MATLAB
 %training parameters in deep network as in this layer
 digtargets_trn = convertDigits(digtargets_trn);
-softnet = trainSoftmaxLayer(features3,digtargets_trn,'MaxEpochs',epochs);
+class_epochs = 75;
+softnet = trainSoftmaxLayer(features3,digtargets_trn,'MaxEpochs',class_epochs);
 
 %% Training of the network
 %stack the different prelearned layers MATLAB
@@ -52,7 +53,7 @@ deepnet = stack(autoenc1,autoenc2,autoenc3,softnet);
 %[W, V, MMSE] = trainMultiLayerDelta(features3',digtargets_trn,eta,epochs);
 
 %Train the whole network together MATLAB
-%deepnet = train(deepnet,bindata_trn,digtargets_trn);
+deepnet = train(deepnet,bindata_trn,digtargets_trn);
 toc
 %% Testing the trained deep autoencoder
 
@@ -63,16 +64,17 @@ toc
 %decode data with the last autoencoder
 %test_features = decode(autoenc3,test_features3);
 
-output_digits = deepnet(bindata_tst);
-
 %output_digits = feedforwardMultiLayerDelta(test_features3', W, V);
 
 %class = sum(output_digits == digtargets_tst');
+output_trn = deepnet(bindata_trn);
+output_tst = deepnet(bindata_tst);
 
-%convert output
-%output_digits = convertDigits(output_digits');
 %convert targets
 digtargets_tst = convertDigits(digtargets_tst);
 
 %plot classification performance
-plotconfusion(digtargets_tst,output_digits);
+figure(4);
+plotconfusion(digtargets_trn,output_trn);
+figure(5)
+plotconfusion(digtargets_tst,output_tst);
